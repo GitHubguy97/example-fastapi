b35755461804dfc08b85a1c5307f4eda504af110
Gideon Jamala <Gideon.living@gmail.com>
2024-09-19 14:13:36 -0400

    Corrected Secrete spelling

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     database_password: str
     database_username: str  # This must match the corrected env var in .env
     database_name: str  # Add this since it's in the .env file
-    secret_key: str  # Fix the typo from `secrete_key` to `secret_key`
+    secrete_key: str  # Fix the typo from `secrete_key` to `secret_key`
     algorithm: str
     access_token_expire_minutes: int
 
